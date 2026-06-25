47f6b35ecca32b94b604086d0199ce268a8e0102
Manoj Sunar <mutipletask@gmail.com>
2026-06-25 20:14:34 +0545

    add connect server into frontend

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const URL ="https://portfolio-server-ofgs.onrender.com/api";
 
 // Create a configured Axios instance
 const api = axios.create({
