65f71f3108372acc53bd34f6f1d944939afa1e29
Manoj Sunar <mutipletask@gmail.com>
2026-06-25 12:02:27 +0545

    integrate live backend url into the frontend

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create a configured Axios instance
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://my-portfolio-j9yd.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
